2761f714b57b7e047a47934ba0ccaa9d8d3bd929
Flavien Rodrigues <rodrigues.flavien@gmail.com>
2015-05-19 16:47:48 +0200

    last update multiple event condition

--- src/gpio.js
+++ src/gpio.js
@@ -9,13 +9,11 @@ module.exports = function(Gpio){
 
 	Gpio.prototype.getMoisture = function() {
 		var myMoistureObj = new grove_moisture.GroveMoisture(parseInt(this.pin));
-		console.log(this.name+" : "+parseInt(myMoistureObj.value()));
 		return parseInt(myMoistureObj.value());
 	};
 
 	Gpio.prototype.getLight = function() {
 		var light = new groveSensor.GroveLight(parseInt(this.pin))
-		console.log(this.name+" : "+light.value());
 		return light.value();
 	};
 }
