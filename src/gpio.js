50a5d8da02fff3d55630df63c23022e14e529566
Flavien Rodrigues <rodrigues.flavien@gmail.com>
2015-05-19 13:56:21 +0200

    test2

--- src/gpio.js
+++ src/gpio.js
@@ -9,14 +9,14 @@ module.exports = function(Gpio){
 
 	Gpio.prototype.getMoisture = function() {
 		console.log('ok before get value Moisture');
-		var myMoistureObj = new grove_moisture.GroveMoisture(parseInt(Gpio.pin));
+		var myMoistureObj = new grove_moisture.GroveMoisture(parseInt(this.pin));
 		console.log('ok after get value Moisture');
 		return parseInt(myMoistureObj.value());
 	};
 
 	Gpio.prototype.getTemperature = function() {
-console.log('ok before get value temp');
-		var temp = new groveSensor.GroveTemp(parseInt(Gpio.pin));
+		console.log('ok before get value temp');
+		var temp = new groveSensor.GroveTemp(parseInt(this.pin));
 		console.log('ok after get value temp');
 		return temp.value();
 	};
