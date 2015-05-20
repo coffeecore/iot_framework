fe56f09d28bcec931ff0e5ee4069e2df0f2115bb
Flavien Rodrigues <rodrigues.flavien@gmail.com>
2015-05-20 12:53:10 +0200

    fd

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express    = require('express');
 var bodyParser = require("body-parser");
 var app        = express();
 
+app.use(bodyParser.urlencoded());
 app.use(bodyParser.json());
 
 var file_object = require('./conf/thing');
