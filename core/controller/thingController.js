53fd485db92ce5630948223903a3b9b74e55432d
Flavien Rodrigues <rodrigues.flavien@gmail.com>
2015-05-20 15:31:50 +0200

    ctrl

--- core/controller/thingController.js
+++ core/controller/thingController.js
@@ -55,48 +55,20 @@ module.exports = function(app){
 
 					if(g.description != gpio.description){
 						gpio.description = g.description;
-					}		
+					}
+
+					gpio.events.forEach(function(evt){
+						g.events.forEach(function(e){
+							if(evt.id == e.id){
+								if(e.name != evt.name) {
+									evt.name = e.name;
+								}								
+							}
+						});
+					});
 				}
 			});
 		});
-
-		// thing.gpios.forEach(function(gpio){
-		// 	app.Thing.gpios.forEach(function(g){
-		// 		if(g.id == gpio.id) {
-
-		// 			if(g.name != gpio.name) {
-		// 				g.name = gpio.name;
-		// 			}
-
-		// 			if(g.pin != gpio.pin){
-		// 				g.pin = gpio.pin;
-		// 			}
-
-		// 			if(g.description != gpio.description){
-		// 				g.description = gpio.description;
-		// 			}
-
-		// 			gpio.events.forEach(function(evt){
-		// 				g.events.forEach(function(e){
-		// 					if(evt.id == e.id) {
-		// 						if(evt.name != e.name) {
-		// 							g.name = evt.name;
-		// 						}
-		// 					}
-		// 				});
-		// 			});
-		// 			// gpio.jobs.forEach(function(job){
-		// 			// 	g.jobs.forEach(function(j){
-		// 			// 		if(job.id == j.id) {
-		// 			// 			if(job.name != j.name) {
-		// 			// 				j.name = job.name;
-		// 			// 			}
-		// 			// 		}
-		// 			// 	});
-		// 			// });
-		// 		}
-		// 	});
-		// });
 
 		app.Thing.save();
 	}
