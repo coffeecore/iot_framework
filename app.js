ef137773ce64e4e9572ef2fa7cc4494aaadb973b
Flavien Rodrigues <rodrigues.flavien@gmail.com>
2015-05-20 12:33:11 +0200

    body parser

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var app = express();
+var express    = require('express');
+var bodyParser = require("body-parser");
+var app        = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
