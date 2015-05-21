c80b8c253c674c7c24972f66c59b0106c2346533
Flavien Rodrigues <rodrigues.flavien@gmail.com>
2015-05-21 14:17:23 +0200

    header responses

--- core/routes.js
+++ core/routes.js
@@ -34,10 +34,7 @@ module.exports = function(app){
     // Describe the GPIO
     app.get('/thing/gpio/:slug', function(req, res){
         var r = app.thing_gpio_nameAction(req);
-
-        res.status(r.status_code);
-
-        res.json(r);
+        res.status(r.status_code).json(r);
     });
 
     // List all the GPIO's events
